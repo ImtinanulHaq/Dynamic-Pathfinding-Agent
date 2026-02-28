e52f96d0c14fa0be540b83aff765409018f0da6b
Presentation Developer <developer@presentation-slides.com>
2026-02-28 07:19:09 +0500

    Add simple MapEditor for toggling obstacles

--- grid/editor.py
+++ grid/editor.py
@@ -1,1 +1,10 @@
-# Interactive map editor logic will be implemented here
+
+class MapEditor:
+	def __init__(self, grid):
+		self.grid = grid
+
+	def toggle_obstacle(self, row, col):
+		if self.grid.is_obstacle(row, col):
+			self.grid.remove_obstacle(row, col)
+		else:
+			self.grid.add_obstacle(row, col)
