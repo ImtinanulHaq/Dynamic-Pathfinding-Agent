dea26ce2ec0d8ec136f6a7694a83f0c1c311ecc6
Presentation Developer <developer@presentation-slides.com>
2026-02-28 07:20:55 +0500

    Add simple A* algorithm implementation (manhattan/euclidean)

--- algorithms/astar/astar.py
+++ algorithms/astar/astar.py
@@ -1,1 +1,29 @@
-# A* algorithm implementation will be here
+
+import heapq
+from algorithms.heuristics import manhattan, euclidean
+
+def astar(grid, heuristic='manhattan'):
+	start = grid.start
+	goal = grid.goal
+	if heuristic == 'manhattan':
+		h = manhattan
+	else:
+		h = euclidean
+
+	open_set = []
+	heapq.heappush(open_set, (0 + h(start, goal), 0, start, [start]))
+	visited = set()
+
+	while open_set:
+		f, g, current, path = heapq.heappop(open_set)
+		if current == goal:
+			return path
+		if current in visited:
+			continue
+		visited.add(current)
+		for d in [(-1,0),(1,0),(0,-1),(0,1)]:
+			nr, nc = current[0]+d[0], current[1]+d[1]
+			if 0 <= nr < grid.rows and 0 <= nc < grid.cols:
+				if not grid.is_obstacle(nr, nc) and (nr, nc) not in visited:
+					heapq.heappush(open_set, (g+1+h((nr,nc), goal), g+1, (nr,nc), path+[(nr,nc)]))
+	return None
