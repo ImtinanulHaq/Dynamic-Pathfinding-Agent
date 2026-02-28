41c480472e2ae7f3b1ce6acdfb6bd9d28e5d6d9e
Presentation Developer <developer@presentation-slides.com>
2026-02-28 07:19:38 +0500

    Add simple Agent class for position and movement

--- agent/agent.py
+++ agent/agent.py
@@ -1,1 +1,9 @@
-# Agent logic will be implemented here
+
+class Agent:
+	def __init__(self, grid):
+		self.grid = grid
+		self.position = grid.start
+
+	def move_to(self, row, col):
+		if not self.grid.is_obstacle(row, col):
+			self.position = (row, col)
