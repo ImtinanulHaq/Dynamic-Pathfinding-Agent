b5ee88cc712f1499f9f233b1e6f68387f6098dc9
Presentation Developer <developer@presentation-slides.com>
2026-02-28 07:23:18 +0500

    Add simple Metrics class for nodes visited, path cost, and execution time

--- metrics/metrics.py
+++ metrics/metrics.py
@@ -1,1 +1,24 @@
-# Metrics dashboard logic will be implemented here
+
+import time
+
+class Metrics:
+	def __init__(self):
+		self.nodes_visited = 0
+		self.path_cost = 0
+		self.start_time = 0
+		self.end_time = 0
+
+	def start_timer(self):
+		self.start_time = time.time()
+
+	def stop_timer(self):
+		self.end_time = time.time()
+
+	def get_execution_time(self):
+		return (self.end_time - self.start_time) * 1000
+
+	def reset(self):
+		self.nodes_visited = 0
+		self.path_cost = 0
+		self.start_time = 0
+		self.end_time = 0
